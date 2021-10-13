09cc619ad6421b16efe6472a15c50046eb411ffd
tyort <neotyo1986@gmail.com>
2021-10-13 10:27:53 +0300

    4.10. Удалит служебные маршруты.
    В служебных маршрутах больше нет необходимости. Удалим их.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,15 +1,13 @@
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import {AppRoute, AuthorizationStatus} from '../../const';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
-import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
-import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import AuthScreen from '../auth-screen/auth-screen';
 import GameOverScreen from '../game-over-screen/game-over-screen';
 import WinScreen from '../win-screen/win-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 import PrivateRoute from '../private-route/private-route';
 import GameScreen from '../game-screen/game-screen';
-import {Questions, QuestionGenre, QuestionArtist} from '../../types/question';
+import {Questions} from '../../types/question';
 
 // Названия свойств типа должны совпадать с пропсами родителя
 type AppScreenProps = {
@@ -18,8 +16,6 @@ type AppScreenProps = {
 }
 
 function App({errorsCount, questions}: AppScreenProps): JSX.Element {
-  const [firstQuestion, secondQuestion] = questions;
-
   return (
     <BrowserRouter>
       {/* Switch позволяет выбирать один из всех Route */}
@@ -27,23 +23,6 @@ function App({errorsCount, questions}: AppScreenProps): JSX.Element {
         <Route exact path={AppRoute.Root}>
           <WelcomeScreen
             errorsCount={errorsCount}
-          />
-        </Route>
-        <Route exact path={AppRoute.DevArtist}>
-          <ArtistQuestionScreen
-            question={secondQuestion as QuestionArtist}
-            onAnswer={() => {
-              throw new Error('Function \'onAnswer\' isn\'t implemented.');
-            }}
-          />
-        </Route>
-        <Route exact path={AppRoute.DevGenre}>
-          <GenreQuestionScreen
-            // as - явно преобразуем объект к типу
-            question={firstQuestion as QuestionGenre}
-            onAnswer={() => {
-              throw new Error('Function \'onAnswer\' isn\'t implemented.');
-            }}
           />
         </Route>
         <Route exact path={AppRoute.Login}>

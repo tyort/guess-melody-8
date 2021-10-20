5138ba9df4f691b350c3b34a38c64b65b991b0a7
tyort <neotyo1986@gmail.com>
2021-10-20 18:57:05 +0300

    6.3. Добавит поддержку инструмента Redux DevTools.
    Для отладки redux-приложений есть замечательный инструмент
    «Redux DevTools» (поставляется в виде расширения для браузера). Добавим
    его поддержку. Для этого необходимо немного скорректировать
    инициализацию хранилища.
    
    Упростить включение поддержки инструмента «Redux DevTools» поможет
    дополнительный пакет — `redux-devtools-extenstion`. Установим его из
    npm.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux'; // мостик между React и Redux
 import App from './components/app/app';
 import {questions} from './mocks/questions';
 import {reducer} from './store/reducer';
@@ -10,8 +11,10 @@ const Setting = {
   ERRORS_COUNT: 3,
 };
 
-const store = createStore(reducer);
-
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store = {store}>

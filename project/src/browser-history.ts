36d630045c92eeca3f2610413d26e9f94618e1a4
tyort <neotyo1986@gmail.com>
2021-10-27 23:44:24 +0300

    7.13. Создадим новый экземпляр объекта для работы с History API.
    Пока вся маршрутизация в приложении осуществляется с помощью компонентов.
    Для перенаправления у нас есть `Redirect`, а заодно служебный пропс,
    предоставляющий доступ к обёртке над History API.
    
    Но как быть если требуется выполнить перенаправление по какому-либо
    маршруту не из компонента? Например, из действия. Компонент `Redirect`
    нам тут не поможет. Служебный проп тоже.
    
    Для этого мы можем воспользоваться пакетом `history`. React router зависит
    от этого пакета, но не предоставляет возможности использовать его снаружи.
    
    Создадим модуль `browser-history.js` и напишем код
    инициализации нового экземпляра объекта для работы с History API.

---
+++ project/src/browser-history.ts
@@ -0,0 +1,6 @@
+// createBrowserHistory - конфигуратор объекта history
+import {createBrowserHistory} from 'history';
+
+const browserHistory = createBrowserHistory();
+
+export default browserHistory;
